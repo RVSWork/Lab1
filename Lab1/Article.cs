a45ac071e560c4552743d3523811678c220eb6cf
sofysmol <sov-95@mail.ru>
2015-10-27 12:25:36 +0300

    Добавлен Класс Article

---
+++ Lab1/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Article
+    {
+        public Article(String key) { Key = key; }
+        private String Key;
+        private String Content;
+        public String getKey(){ return Key; }
+        public String getContent() { return Content; }
+        public void setContent(String content) { Content = content; }
+    }
+}
