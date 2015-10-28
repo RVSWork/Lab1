da7956da5cb2e39f30aad0e00a947307e0e7eeca
Kris-Peach <Zhozefi@yandex.ru>
2015-10-29 00:04:39 +0300

    Форма для редактирования статьи

---
+++ Editor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Editor : Form
+    {
+        public Editor(string header)
+        {
+            InitializeComponent();
+            textBox1.Text = header;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+
+        }
+        public string ReturnData()
+        {
+            return (textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
