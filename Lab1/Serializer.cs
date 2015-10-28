04c309bb58aa7897d3515447fa09b632a4bf7acb
sofysmol <sov-95@mail.ru>
2015-10-28 14:02:56 +0300

    Добавлен класс Serializer

---
+++ Lab1/Serializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Serializer
+    {
+        private static Message ByteArrayToMessage(byte[] arrBytes)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Message obj = (Message)binForm.Deserialize(memStream);
+            return obj;
+        }
+        private static byte[] MessageToByteArray(Message obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+            return ms.ToArray();
+        }
+
+    }
+}
