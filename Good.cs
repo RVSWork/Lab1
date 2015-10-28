dd20a40868e689dae218719b0cbc7d5802e3e796
Kris-Peach <Zhozefi@yandex.ru>
2015-10-29 00:20:43 +0300

    Форма для оповещения о хорошем выполнении процесса

---
+++ Good.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Good : Form
+    {
+        public Good(string str)
+        {
+            InitializeComponent();
+            label1.Text = str;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
