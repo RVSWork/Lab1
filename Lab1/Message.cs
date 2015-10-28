63f36a35286e7b814c24d38d6c2c4f377ca8e6f6
sofysmol <sov-95@mail.ru>
2015-10-28 11:10:37 +0300

    Созданны первые версии класса Server и Message. В Message в коментариях условные обозначения.

---
+++ Lab1/Message.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Message
+    {
+        private
+            Article mArticle;
+            int Code_mode;
+            int Code_status;
+            String Key_article;
+            String Name_addressee;
+            String Name_sender;
+        public Message(){ }
+        public void setArticle(Article article) { mArticle = article; }
+        public void setKey(String key) { Key_article = key; }
+        public void setCodeMode(int code) { Code_mode = code; }
+        public void setCodeStatus(int code) { Code_status = code; }
+        public void setAddressee(String addressee) { Name_addressee = addressee; }
+        public void setSender(String sender) { Name_sender = sender; }
+
+        public Article getArticle() { return mArticle; }
+        public String getKey() { return Key_article; }
+        public String getAdressee() { return Name_addressee; }
+        public String getSender() { return Name_sender; }
+    }
+
+    /*
+    Code_mode
+    ------------------
+    0-читать
+    1-изменять
+    2-создавать
+    3-завершить
+    4-сохранить
+    ------------------
+    Code_status
+    ------------------
+   200-ОК
+   404-Запись не сущуствует
+   405-Запись уже существует
+   406-Запись нельзя редактировать
+    */
+}
---
+++ Lab1/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    class Server
+    {
+        private Hashtable List_backup_copies;
+        private Hashtable List_editable_copies;
+        public Server() {
+            List_backup_copies = new Hashtable();
+            List_editable_copies = new Hashtable();
+        }
+
+        
+        public void addArticle() { }
+        public void addBackupCopy() { }
+        public void addEditableCopy() { }
+        public void deleteArticle() { }
+        public void deleteBackupCopy() { }
+        public void deleteEditableCopy() { }
+        public Article getArticle(String name) { return new Article(name); }
+        public void getMsg(Socket handler) {
+            String data = null;
+            byte[] bytes = new byte[1024];
+            int bytesRec = handler.Receive(bytes);
+            data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+            processingMsg(data);
+        }
+        public bool keepArticle() { return false; }
+        public void memberOfBackup() { }
+        public void memberOfEditable() { }
+        public void processingMsg(String data) {
+            switch (data.Length)
+            {
+                case
+            }
+        }
+        public void saveArticle() { }
+        public void sendMsg() { }
+
+
+
+    }
+}
