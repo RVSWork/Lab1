720e1ffc94378ec930a15e04a96ddd39bfcb6444
Kris-Peach <Zhozefi@yandex.ru>
2015-12-10 22:21:22 +0300

    Формочка клиента SOAP

---
+++ WebPageSeparated.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebPageSeparated.aspx.cs" Inherits="WebPageSeparated" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" type="text/css" href="Content/StyleSheet.css"/>
+
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="Conteiner" class="conteiner">
+        <div id="Header" class="header">
+            <p><h2>Добро пожаловать в Справочник МОЭ</h2></p><br />
+            <p><b>Название статьи:</b> <asp:TextBox ID="TextBox1" runat="server" Height="20px" Width="219px" BorderStyle="Dotted" Font-Size="Large"></asp:TextBox>
+        <asp:Button ID="Button2" runat="server" Text="Редактировать" OnClick="Button2_Click" CssClass="button2" />
+                <br /></p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Читать" CssClass="button1" />
+        <asp:Button ID="Button3" runat="server" Text="Создать новую" OnClick="Button3_Click" CssClass="button3" />
+            </div>
+        
+        
+        <br />
+        <br />
+        <br />
+        <div id="content">
+            <asp:Label ID="Label1" runat="server" Text="Статья" CssClass="lable" Font-Size="X-Large"></asp:Label><br />
+        <br />
+            <asp:TextBox ID="TextBox2" runat="server" style="margin-top: 0px; top: 274px; left: 137px; height: 301px; right: 529px; width: 323px;" ReadOnly="True" TextMode="MultiLine" CssClass="text" Font-Size="X-Large"></asp:TextBox>
+        </div>
+        
+        
+        
+        
+       
+    
+        <asp:Button ID="Button4" runat="server" Height="43px" OnClick="Button4_Click" Text="Сохранить" Visible="False" CssClass="button4" Width="135px" />
+        <asp:Button ID="Button5" runat="server" Height="43px" OnClick="Button5_Click" style="margin-left: 330px; top: 296px; left: 464px;" Text="Завершить" Visible="False" Width="135px" CssClass="button5" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebPageSeparated.aspx.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ServiceReference1;
+
+public partial class WebPageSeparated : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private class Info //Вложеный класс, который содержит информацию для соединения
+    {
+        public string key;
+        public string content;
+        public int code_mode;
+        public SOAPServerSoapClient client;
+        public Info(string k, int code, string con)
+        {
+            key = k;
+            code_mode = code;
+            content = con;
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)//читать
+    {
+        Info info = new Info(TextBox1.Text,0,TextBox2.Text);
+        info.client = new SOAPServerSoapClient();
+        
+        Message1 answer = new Message1();
+        answer.Code_status = 200;
+        while(true)
+        {
+            if (info.key != null)
+            { answer = info.client.MsgToClient(info.key, info.code_mode, info.content);
+                break;
+            }
+            else { Label1.Text = "Укажите название статьи!!"; }
+        }
+        
+        if (answer.Code_status == 404)
+        {
+            TextBox2.Text = "";
+            Label1.Text = "Запись не существует!"; }
+        else
+        {
+            TextBox2.Text = "";
+            Label1.Text = info.key;
+            TextBox2.Text = answer.Article.Content;
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e) //редактировать
+    {
+        Info info = new Info(TextBox1.Text, 1, TextBox2.Text);
+        info.client = new SOAPServerSoapClient();
+        Message1 answer = new Message1();
+        answer.Code_status = 200;
+        while (true)
+        {
+            if (info.key != null)
+            {
+                answer = info.client.MsgToClient(info.key, info.code_mode, info.content);
+                break;
+            }
+            else { Label1.Text = "Укажите название статьи!!"; }
+        }
+        if (answer.Code_status == 406 || answer.Code_status == 404)
+        {
+            TextBox2.Text = "";
+            Label1.Text = "Запись нельзя редактировать!"; }
+        else
+        {
+            //редактирвать можно
+            Button4.Visible = true;
+            Button5.Visible = true;
+            Button1.Visible = false;
+            Button2.Visible = false;
+            Button3.Visible = false;
+            TextBox2.ReadOnly = false;
+            Label1.Text = info.key;
+            TextBox2.Text = answer.Article.Content;
+        }
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)//сохранить
+    {
+        Info info = new Info(TextBox1.Text, 4, TextBox2.Text);
+        info.client = new SOAPServerSoapClient();
+        Message1 answer = new Message1();
+        answer.Code_status = 200;
+        answer = info.client.MsgToClient(info.key, info.code_mode, info.content);
+        if (answer.Code_status == 200)
+        {
+            Label1.Text = "Сохранено!";
+        }
+        else { Label1.Text = "Не удалось!"; }
+    }
+
+    protected void Button5_Click(object sender, EventArgs e) //завершить редактирование
+    {
+        Info info = new Info(TextBox1.Text, 3, TextBox2.Text);
+        info.client = new SOAPServerSoapClient();
+        Message1 answer = new Message1();
+        answer.Code_status = 200;
+        answer = info.client.MsgToClient(info.key, info.code_mode, info.content);
+        if (answer.Code_status == 200)
+        {
+            Button4.Visible = false; Button5.Visible = false;
+            Button3.Visible = true;  Button2.Visible = true;
+            Button1.Visible = true;  TextBox2.ReadOnly = true;
+            TextBox1.Text = "";  TextBox2.Text = ""; Label1.Text = "";
+        }
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)//создать новую
+    {
+        Info info = new Info(TextBox1.Text, 2, TextBox2.Text);
+        info.client = new SOAPServerSoapClient();
+        Message1 answer = new Message1();
+        answer.Code_status = 200;
+        while (true)
+        {
+            if (info.key != null)
+            {
+                answer = info.client.MsgToClient(info.key, info.code_mode, info.content);
+                break;
+            }
+            else { Label1.Text = "Укажите название статьи!!"; }
+        }
+        if (answer.Code_status != 200)
+        {
+            if(answer.Code_status == 405) Label1.Text = "Запись уже сущетвует!!";
+            if (answer.Code_status == 406) Label1.Text = "Создать невозможно!!";
+        }
+        else
+        {//можно создавать
+            Button4.Visible = true;
+            Button5.Visible = true;
+            Button1.Visible = false;
+            Button2.Visible = false;
+            Button3.Visible = false;
+            TextBox2.Text = ""; Label1.Text = info.key;
+            TextBox2.ReadOnly = false;
+        }
+
+    }
+}
